318c4b7a66e1a981502c8aa7b839dc8e8d373644
Hector Vega <linnk99@gmail.com>
2020-07-08 22:43:40 -0500

    Preparing aliens for the game

--- chapter_12/alien_invasion/alien_invasion.py
+++ chapter_12/alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -20,9 +21,18 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # set background color
         self.bg_color = (128, 223, 255)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -90,6 +100,10 @@ class AlienInvasion:
 
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # make a game instance and run the game
--- chapter_12/alien_invasion/settings.py
+++ chapter_12/alien_invasion/settings.py
@@ -10,10 +10,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # ship settings
-        self.ship_speed = 5
+        self.ship_speed = 10
 
         # bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 10
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
