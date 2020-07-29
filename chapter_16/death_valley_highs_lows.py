d1a9439cc4fc4479c934fc5bf7a0f730aa26ecba
Hector Vega <linnk99@gmail.com>
2020-07-28 23:21:15 -0500

    CSV files added

---
+++ chapter_16/death_valley_highs_lows.py
@@ -0,0 +1,36 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/sitka_weather_2018_simple.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # get dates, high amd low temperatures from this file
+    dates, highs, lows = [], [], []
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# plot the high temperatures
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c = 'red', alpha = 0.5)
+ax.plot(dates, lows, c = 'blue', alpha = 0.5)
+ax.fill_between(dates, highs, lows, facecolor = 'green', alpha = 0.1)
+
+# format plot
+ax.set_title("Daily high and low temperatures - 2018", fontsize = 24)
+ax.set_xlabel('', fontsize = 16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize = 16)
+ax.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+plt.show()
