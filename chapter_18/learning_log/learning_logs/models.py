842c1b9c84fb5d11287b5f329210c810f7cc1dab
Hector Vega <linnk99@gmail.com>
2020-08-19 23:16:14 -0500

    Excercise 18-2 completed to add short entries

--- chapter_18/learning_log/learning_logs/models.py
+++ chapter_18/learning_log/learning_logs/models.py
@@ -23,4 +23,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """Returns a string representation of the model."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
