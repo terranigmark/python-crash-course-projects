bd4ca3940c00b4b2e8e648043bad5da241ee1129
Hector Vega <linnk99@gmail.com>
2020-08-20 23:47:40 -0500

    Superuser and name of pizzas added

--- chapter_18/pizzeria/pizzeria_app/models.py
+++ chapter_18/pizzeria/pizzeria_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Pizza(models.Model):
     """A pizza included in the restaurant menu"""
-    description = models.CharField(max_length = 150)
+    name = models.CharField(max_length = 150)
     date_added = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.description
+        return self.name
