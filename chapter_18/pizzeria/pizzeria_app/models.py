e49dfbfe80c01a17cbcac0482b87b6155178724a
Hector Vega <linnk99@gmail.com>
2020-08-20 23:53:50 -0500

    Model created for pizzas descriptions

--- chapter_18/pizzeria/pizzeria_app/models.py
+++ chapter_18/pizzeria/pizzeria_app/models.py
@@ -9,3 +9,16 @@ class Pizza(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
+
+class Description(models.Model):
+    """Description of the pizzas"""
+    description = models.ForeignKey(Pizza, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'descriptions'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
